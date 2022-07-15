9c1a6bace9375f8af7c4cd8f969d69122ff43982
Constantine <koskash98ksband@gmail.com>
2022-07-15 16:38:21 +0600

    HW Done

---
+++ Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+Console.Write("Введите количество чисел ");
+int elCount = int.Parse(Console.ReadLine());
+int count = 0;
+int[] mass = new int[elCount];
+for (int i = 0; i < elCount; i++)
+{
+    Console.Write($"Введите число {i + 1}, осталось ввести {elCount - i} ");
+    mass[i] = int.Parse(Console.ReadLine());
+    if (mass[i] > 0) count++;
+}
+System.Console.WriteLine($"Чисел больше 0 - {count}");
+
+
+
+
+
